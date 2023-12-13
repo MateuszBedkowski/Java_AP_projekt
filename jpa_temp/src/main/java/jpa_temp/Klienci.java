d63a749ca2c296be6a4dd96ec451f3f8d6cb10cf
Mateusz Bedkowski <stoper.zaglebie@gmail.com>
2023-12-13 20:58:09 +0100

    added style.css, will work on views on weekend

---
+++ jpa_temp/src/main/java/jpa_temp/Klienci.java
@@ -0,0 +1,70 @@
+package jpa_temp;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the klienci database table.
+ * 
+ */
+@Entity
+@Table(name="klienci")
+@NamedQuery(name="Klienci.findAll", query="SELECT k FROM Klienci k")
+public class Klienci implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int idklient;
+
+	private String imie;
+
+	private String nazwisko;
+
+	private String pesel;
+
+	private String telefon;
+
+	public Klienci() {
+	}
+
+	public int getIdklient() {
+		return this.idklient;
+	}
+
+	public void setIdklient(int idklient) {
+		this.idklient = idklient;
+	}
+
+	public String getImie() {
+		return this.imie;
+	}
+
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+
+	public String getNazwisko() {
+		return this.nazwisko;
+	}
+
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+
+	public String getPesel() {
+		return this.pesel;
+	}
+
+	public void setPesel(String pesel) {
+		this.pesel = pesel;
+	}
+
+	public String getTelefon() {
+		return this.telefon;
+	}
+
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+
+}
---
+++ jpa_temp/src/main/java/jpa_temp/Role.java
@@ -0,0 +1,69 @@
+package jpa_temp;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the role database table.
+ * 
+ */
+@Entity
+@Table(name="role")
+@NamedQuery(name="Role.findAll", query="SELECT r FROM Role r")
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idrola;
+
+	private String nazwa;
+
+	//bi-directional many-to-one association to Pracownicy
+	@OneToMany(mappedBy="role")
+	private List<Pracownicy> pracownicies;
+
+	public Role() {
+	}
+
+	public int getIdrola() {
+		return this.idrola;
+	}
+
+	public void setIdrola(int idrola) {
+		this.idrola = idrola;
+	}
+
+	public String getNazwa() {
+		return this.nazwa;
+	}
+
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+
+	public List<Pracownicy> getPracownicies() {
+		return this.pracownicies;
+	}
+
+	public void setPracownicies(List<Pracownicy> pracownicies) {
+		this.pracownicies = pracownicies;
+	}
+
+	public Pracownicy addPracownicy(Pracownicy pracownicy) {
+		getPracownicies().add(pracownicy);
+		pracownicy.setRole(this);
+
+		return pracownicy;
+	}
+
+	public Pracownicy removePracownicy(Pracownicy pracownicy) {
+		getPracownicies().remove(pracownicy);
+		pracownicy.setRole(null);
+
+		return pracownicy;
+	}
+
+}
